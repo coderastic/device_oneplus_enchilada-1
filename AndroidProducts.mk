3dc30b727b0e0ca49874262ad8f3a489efc4c6b9
Shubham Mapari <36085347+coderastic@users.noreply.github.com>
2020-04-11 13:09:54 -0700

    superior bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_enchilada.mk
+    $(LOCAL_DIR)/superior_enchilada.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_enchilada-user \
-    aosp_enchilada-userdebug \
-    aosp_enchilada-eng
+    superior_enchilada-user \
+    superior_enchilada-userdebug \
+    superior_enchilada-eng
